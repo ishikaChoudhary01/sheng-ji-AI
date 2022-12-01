a287a9105db8606e290e1b35f5c888c016bc156f
Ishika Choudhary <ishika6262@gmail.com>
2022-11-30 23:56:50 -0500

    starting env folder

---
+++ rlcard-master/rlcard/envs/sheng_ji.py
@@ -0,0 +1,52 @@
+from rlcard.envs import Env
+
+
+class ShengJiEnv(Env):
+
+    def __init__(self):
+        self.name = "sheng ji"
+        self.game = ShengJiGame()
+
+
+    def get_payoffs(self):
+        ''' Get the payoffs of players.
+
+        Returns:
+            (list): A list of payoffs for each player.
+
+        Note: Must be implemented in the child class.
+        '''
+        raise NotImplementedError
+
+
+    def _extract_state(self, state):
+        ''' Extract useful information from state for RL.
+
+        Args:
+            state (dict): The raw state
+
+        Returns:
+            (numpy.array): The extracted state
+        '''
+        raise NotImplementedError
+
+    def _decode_action(self, action_id):
+        ''' Decode Action id to the action in the game.
+
+        Args:
+            action_id (int): The id of the action
+
+        Returns:
+            (string): The action that will be passed to the game engine.
+
+        '''
+        raise NotImplementedError
+
+    def _get_legal_actions(self):
+        ''' Get all legal actions for current state.
+
+        Returns:
+            (list): A list of legal actions' id.
+
+        '''
+        raise NotImplementedError
--- rlcard-master/rlcard/games/sheng_ji/__init__.py
+++
@@ -1,3 +0,0 @@
-from .dealer import ShengJiDealer as Dealer
-from .game import ShengJiGame as Game
-from .player import ShengJiPlayer as Player
